a203a0eb2dc48592608e7e7443ccfea76ca2ab0b
SpinnelSun <matheus.santos@ccc.ufcg.edu.br>
2018-05-18 15:33:20 -0300

    Definição de Structs

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+struct Word {
+    string text;
+    string theme;
+    int level;
+};
+
+struct Player {
+    string name;
+    int score;
+};
+
+vector<Word> words;
+
+void setUpWords() {
+    words.push_back({"-", "-", 1});
+}
+
+
+
 int main() {
-    cout << "Hello world!" << endl;
+    FILE *file = fopen("words.txt", "w");
+    setUpWords();
+
+    for (int i = 0; i < words.size(); i++) {
+        cout << words[i].text << " " << words[i].theme << " " << words[i].level << endl;
+    }
+    cout << "\n";
+
+    fclose(file);
     return 0;
 }
+
