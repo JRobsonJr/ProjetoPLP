aac4ab575c140e41ff8150feaacc50a3cf3bd1cc
SpinnelSun <m.hollygard@gmail.com>
2018-05-22 00:01:57 -0300

    main.cpp: adição de novos métodos para funcionamento da interface.

--- C++/main.cpp
+++ C++/main.cpp
@@ -38,7 +38,7 @@ void setUpWords() {
 
     Word data;
     string currentLine;
-    ifstream wordsFile(FILENAME_WORDS); // Impedindo compilação
+    ifstream wordsFile(FILENAME_WORDS.c_str());
 
     if (isFileValid(wordsFile)) {
         while (getline(wordsFile, currentLine)) {
@@ -59,7 +59,7 @@ void setUpWords() {
 
 void writeWords(){
 
-    ofstream newWordsFile(FILENAME_WORDS); // Impedindo compilação
+    ofstream newWordsFile(FILENAME_WORDS.c_str());
     if (isFileValid(newWordsFile)) {
         for (int i = 0; i < words.size(); i++) {
 
@@ -73,70 +73,160 @@ void writeWords(){
     }
 }
 
-void normalGameOptions();
-void selectTheme();
-void selectLevel();
+void pause() {
+    system("sleep 5s");
+}
 
-void competitiveGame();
-void getPlayerData();
+
 
 int getOption() {
     int option;
 
     cout << endl << endl;
-    cout << "Informe o número da opção desejada: ";
+    cout << "                    Informe o número da opção desejada: ";
     cin >> option;
 
     return option;
 }
 
-void selectOptionGameMode(int option) {
+void showThemes() {
+    system("clear");
+
+    cout << endl;
+    cout << "----------------------------     SELECIONAR TEMA     ---------------------------";
+    cout << endl << endl;
+
+    // LISTAR TEMAS
+
+    // INICIAR PARTIDA
+
+    pause();
+}
+
+void showLevels() {
+    system("clear");
+
+    cout << endl;
+    cout << "------------------------     SELECIONAR DIFICULDADE     ------------------------";
+    cout << endl << endl;
+
+    // LISTAR DIFICULDADES
+
+    // INICIAR PARTIDA
+
+    pause();
+}
+
+void selectFastMatchType(int option) {
     switch (option) {
         case 1:
-            // normalGameOptions();
+            showThemes();
             break;
         case 2:
-            // competitiveGame();
+            showLevels();
             break;
         case 3:
+            // INICIAR PARTIDA
+            break;
+        case 4:
             break;
         default:
-            // INVÁLIDA;
+            // INVÁLIDA
             break;
     }
 }
 
-void selectGameMode()  {
+void fastMatchType()  {
     system("clear");
 
+    cout << endl;
+    cout << "-----------------------------     JOGO RÁPIDO     ------------------------------";
     cout << endl << endl;
-    cout << "----- MODO DE JOGO -----" << endl << endl;
-    cout << "1 - Jogo Rápido" << endl;
-    cout << "2 - Modo Campeonato" << endl;
-    cout << "3 - Voltar" << endl;
 
-    selectOptionGameMode(getOption());
+    cout << "                      Como sua palavra deve ser escolhida?" << endl << endl;
+
+    cout << "                              1  -  Por Tema" << endl;
+    cout << "                              2  -  Por Dificuldade" << endl;
+    cout << "                              3  -  Aleatoriamente" << endl;
+    cout << "                              4  -  Voltar" << endl;
+
+    selectFastMatchType(getOption());
+}
+
+void getPlayerData() {
+    system("clear");
+
+    string nickname;
+
+    cout << endl;
+    cout << "---------------------------     MODO CAMPEONATO     ----------------------------";
+    cout << endl << endl;
+
+    // Como capturar um ESC?
+    cout << "                         (Pressione ESC para voltar...)";
+    cout << endl << endl << endl;
+
+    cout << "                              Insira o seu nick:" << endl << endl << endl;
+    cout << "                                   ";
+    cin >> nickname;
+
+    // CADASTRO DO JOGADOR
+
+    // INICIAR PARTIDA
+}
+
+void selectGameMode(int option) {
+    switch (option) {
+        case 1:
+            fastMatchType();
+            break;
+        case 2:
+            getPlayerData();
+            break;
+        case 3:
+            break;
+        default:
+            // INVÁLIDA
+            break;
+    }
+}
+
+void showGameModes()  {
+    system("clear");
+
+    cout << endl;
+    cout << "-----------------------------     MODO DE JOGO     -----------------------------";
+    cout << endl << endl;
+
+    cout << "                                1  -  Jogo Rápido" << endl;
+    cout << "                                2  -  Modo Campeonato" << endl;
+    cout << "                                3  -  Voltar" << endl;
+
+    selectGameMode(getOption());
 }
 
 void showRules() {
     system("clear");
 
+    cout << endl;
+    cout << "--------------------------------     REGRAS     --------------------------------";
     cout << endl << endl;
-    cout << "----- REGRAS -----" << endl << endl;
-    cout << "    No jogo da forca, o jogador deve acertar a palavra que lhe foi proposta a partir d";
-    cout << "o número de letras dessa palavra e do tema ligado a ela. A cada rodada, o jogador deve";
-    cout << " sugerir uma letra. Se a palavra proposta contiver a letra sugerida, todas as ocorrênc";
-    cout << "ias dessa letra são reveladas. Caso contrário, uma parte do enforcado será desenhada. ";
-    cout << "Para vencer, o jogador deve ser capaz de revelar a palavra por completo antes que todo";
-    cout << " o corpo do enforcado seja desenhado." << endl << "    O jogador poderá se desafiar em";
-    cout << " dois modos distintos: o rápido e o campeonato. No modo rápido, o jogador enfrentará a";
-    cout << "penas uma palavra, podendo especificar a dificuldade ou o tema dela, se assim desejar.";
-    cout << " Já no modo campeonato, o jogador enfrentará diversas palavras em sequência e acumular";
-    cout << "á pontos a cada palavra revelada. Nesse modo, o jogo segue até que o jogador perca uma";
-    cout << " partida ou adivinhe todas as palavras possíveis. Em qualquer um dos casos, seu desemp";
-    cout << "enho será registrado no ranking.";
 
-    getchar();
+    cout << "    No jogo da forca, o jogador deve acertar a palavra que lhe foi proposta a pa"
+         << "rtir do número de letras dessa palavra e do tema ligado a ela. A cada rodada, o "
+         << "jogador deve sugerir uma letra. Se a palavra proposta contiver a letra sugerida,"
+         << " todas as ocorrências dessa letra são reveladas. Caso contrário, uma parte do en"
+         << "forcado será desenhada. Para vencer, o jogador deve ser capaz de revelar a palav"
+         << "ra por completo antes que todo o corpo do enforcado seja desenhado." << endl;
+    cout << "    O jogador poderá se desafiar em dois modos distintos: o rápido e o campeonat"
+         << "o. No modo rápido, o jogador enfrentará apenas uma palavra, podendo especificar "
+         << "a dificuldade ou o tema dela, se assim desejar. Já no modo campeonato, o jogador"
+         << " enfrentará diversas palavras em sequência e acumulará pontos a cada palavra rev"
+         << "elada. Nesse modo, o jogo segue até que o jogador perca uma partida ou adivinhe "
+         << "todas as palavras possíveis. Em qualquer um dos casos, seu desempenho será regis"
+         << "trado no ranking." << endl << endl;
+
+    pause();
 
 }
 
@@ -144,10 +234,11 @@ void showRules() {
 void showRanking() {
     system("clear");
 
+    cout << endl;
+    cout << "--------------------------------     RANKING     -------------------------------";
     cout << endl << endl;
-    cout << "----- RANKING -----" << endl << endl;
 
-    getchar();
+    pause();
 }
 
 void quit() {
@@ -155,37 +246,44 @@ void quit() {
 
     cout << endl << endl;
 
-    cout << "               Até a próxima!" << endl << endl;
+    cout << "                                 Até a próxima!";
+    cout << endl << endl << endl;
 
-    cout << "--------------- DEVELOPED BY ---------------" << endl << endl;
-    cout << "                Fanny Vieira                " << endl;
-    cout << "          Matheus Alves dos Santos          " << endl;
-    cout << "               Misael Augusto               " << endl;
-    cout << "                Paulo Leitão                " << endl;
-    cout << "               Robson Junior                " << endl;
+    cout << "             Paradigmas de Linguagem de Programação - 2018.1 - UFCG";
+    cout << endl << endl << endl;
+
+    cout << "                                DESENVOLVIDO POR:";
     cout << endl << endl;
+
+    cout << "                              Fanny Batista Vieira" << endl;
+    cout << "                            Matheus Alves dos Santos" << endl;
+    cout << "                         Misael Augusto Silva da Costa" << endl;
+    cout << "                            Paulo José Bastos Leitão" << endl;
+    cout << "                       José Robson da Silva Araujo Junior" << endl;
+    cout << endl << endl;
+
 
     system("sleep 3s");
     exit(1);
 }
 
 
-void selectOptionMenu(int option) {
+void selectMenuOption(int option) {
     switch (option) {
         case 1:
-            // selectGameMode();
+            showGameModes();
             break;
         case 2:
-            // showRules();
+            showRules();
             break;
         case 3:
-            // showRanking();
+            showRanking();
             break;
         case 4:
-            // quit();
+            quit();
             break;
         default:
-            // INVÁLIDA;
+            // INVÁLIDA
             break;
     }
 }
@@ -193,18 +291,20 @@ void selectOptionMenu(int option) {
 void showMenu() {
     system("clear");
 
+    cout << endl;
+    cout << "---------------------------------     MENU     ---------------------------------";
     cout << endl << endl;
-    cout << "----- MENU -----" << endl << endl;
-    cout << "1 - Jogar" << endl;
-    cout << "2 - Regras" << endl;
-    cout << "3 - Ranking" << endl;
-    cout << "4 - Sair" << endl;
 
-    selectOptionMenu(getOption());
+    cout << "                                1  -  Jogar" << endl;
+    cout << "                                2  -  Regras" << endl;
+    cout << "                                3  -  Ranking" << endl;
+    cout << "                                4  -  Sair" << endl;
+
+    selectMenuOption(getOption());
 }
 
 // Esperando a veia artística
-void start() {
+void showOpening() {
     system("clear");
 }
 
@@ -212,6 +312,10 @@ void start() {
 int main() {
 
     setUpWords();
+
+    while(true) {
+        showMenu();
+    }
 
     for (int i = 0; i < words.size(); i++) {
         cout << words[i].text << " " << words[i].theme << " " << words[i].level << endl;
