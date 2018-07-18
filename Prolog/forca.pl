86997043dffee40606cd1761693e9fb8c27c66a3
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-07-18 03:09:56 -0300

    Forca.pl: Implementação do setup_words e setup_players

---
+++ Prolog/forca.pl
@@ -0,0 +1,26 @@
+:- use_module(library(apply)).
+:- use_module(library(csv)).
+
+get_rows_data(File, Lists):-
+  csv_read_file(File, Rows, []),
+  rows_to_lists(Rows, Lists).
+
+rows_to_lists(Rows, Lists):-
+  maplist(row_to_list, Rows, Lists).
+
+row_to_list(Row, List):-
+  Row =.. [_|List].
+
+setup_words(Data) :-
+    get_rows_data('../resources/words.csv', Data).
+
+setup_players(Data) :-
+    get_rows_data('../resources/players.csv', Data).
+
+
+:- initialization(main).
+main :-
+    setup_players(X),
+    writeln(X),
+    setup_words(Y),
+    writeln(Y).
