b30e11ecaa01d50085e69410fa0eed92eeac67dc
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-07-25 07:57:58 -0300

    forca.pl: Modifica leitura para base de conhecimento ao invés de listas

--- Prolog/forca.pl
+++ Prolog/forca.pl
@@ -1,47 +1,35 @@
 :- use_module(library(apply)).
 :- use_module(library(csv)).
 
-write_data(File, Data):-
-    open(File, append, Stream),
-    write(Stream, Data),
-    nl(Stream),
-    close(Stream).
+write_word_file :-
+	tell('words.pl'),nl,
+	listing(word/3),
+	told.
 
-read_data(File, Lists):-
-    csv_read_file(File, Rows),
-    rows_to_lists(Rows, Lists).
+write_player_file :-
+	tell('players.pl'),nl,
+	listing(player/2),
+	told.
 
-rows_to_lists(Rows, Lists):-
-    maplist(row_to_list, Rows, Lists).
-
-row_to_list(Row, List):-
-    Row =.. [_|List].
-
-setup_words(Data) :-
-    read_data('../resources/words_lower_case.csv', Data).
+setup_words :-
+    reconsult('words.pl').
 
 setup_players(Data) :-
-    read_data('../resources/players.csv', Data).
+    reconsult('players.pl').
 
 write_word(Text, Theme):-
     get_level(Text, Level),
-    atomic_list_concat([Text, Theme, Level], ',', Data),
-    write_data('../resources/words_lower_case.csv', Data).
+    assertz(word(Text, Theme, Level)),
+    write_word_file.
 
 write_player(Nickname, Score):-
-    atomic_list_concat([Nickname, Score], ',', Data),
-    write_data('../resources/players.csv', Data).
+    assertz(player(Nickname, Level)),
+    write_player_file.
 
 get_themes(Result):-
-    setup_words(Data),
-    get_themes_loop(Data, [], Result).
-
-% Adiciona os temas no inicio da lista
-get_themes_loop([], Themes, Themes).
-get_themes_loop([Head|Tail], Themes, Result):-
-    nth0(1, Head, Elem),
-    (member(Elem, Themes) -> get_themes_loop(Tail, Themes, Result);
-    get_themes_loop(Tail, [Elem|Themes], Result)).
+    setup_words,
+    findall(Theme, word(_, Theme, _), Queries),
+    list_to_set(Queries, Result).
 
 get_level(Text, Level):- 
     atom_length(Text, Size),
@@ -49,22 +37,19 @@ get_level(Text, Level):-
     compare(<, Size, 10) -> Level is 2;
     Level is 3).
     
-filter_words(Criteria, Result):-
-    setup_words(Data),
-    filter_words_loop(Data, Criteria, [], Result).
+filter_by_theme(Theme, Result):-
+    setup_words,
+    findall([Text, Theme, Level], word(Text, Theme, Level), Result).
 
-filter_words_loop([], _, Result, Result).
-filter_words_loop([Head|Tail], Criteria, List, Result):-
-    member(Criteria, Head) -> 
-        append(List, [Head], NewList), 
-        filter_words_loop(Tail, Criteria, NewList, Result);
-    filter_words_loop(Tail, Criteria, List, Result).
+filter_by_level(Level,Result):-
+    setup_words,
+    findall([Text, Theme, Level], word(Text, Theme, Level), Result).
 
 themed_fast_match:- 
     % lower_case
     % chamar metodo com a interface(select_theme),
     read(Theme),
-    filter_words(Theme, Result),
+    filter_by_theme(Theme, Result),
     get_random_word(Result, RandomWord),
     writeln(RandomWord).
     % chamar startGame.
@@ -72,7 +57,7 @@ themed_fast_match:-
 leveled_fast_match:-
     % chamar select_level
     read(Level),
-    filter_words(Level, Result),
+    filter_by_level(Level, Result),
     get_random_word(Result, RandomWord),
     writeln(RandomWord).
     %chamar startGame.
