77ba951c86b603b9bcd8908ae992572162f4e2b8
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-07-25 08:57:25 -0300

    players.pl: Corrige formatação

--- Prolog/players.pl
+++ Prolog/players.pl
@@ -1,9 +1,14 @@
-player(Robson,85).
-player(robson,85),
-player(robson,80),
-player(robson,0),
-player(robson,0),
-player(batata,71),
-player(batata,55),
-player(fanny,50),
-player(fanny,50).
+
+:- dynamic player/2.
+
+player(opa, 80).
+player(robson, 85).
+player(robson, 85).
+player(robson, 80).
+player(robson, 0).
+player(robson, 0).
+player(batata, 71).
+player(batata, 55).
+player(fanny, 50).
+player(fanny, 50).
+
